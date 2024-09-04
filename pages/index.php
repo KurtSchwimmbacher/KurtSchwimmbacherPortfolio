ee56e5df2495bd31588f06eda83c10cddbeb1fdb
KurtSchwimmbacher <231002@virtualwindow.co.za>
2024-09-04 10:48:24 +0200

    add index and header
    add index.php, navbar, and header and begin styling elements on index

---
+++ pages/index.php
@@ -0,0 +1,32 @@
+
+
+<!-- include header -->
+<?php require_once '../includes/header.php';?>
+
+<!-- include nav bar -->
+<?php require_once '../includes/Navbar.php';?>
+
+<!-- link to css -->
+<link href="../css/index.css" rel="stylesheet">
+
+
+<main class="main-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 title-col mt-5">
+                <h1>Take a look at my work</h1>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12">
+                <ul class="secondary-nav mt-5">
+                    <li class="sec-nav-link">UX</li>
+                    <li class="sec-nav-link">Dev</li>
+                    <li class="sec-nav-link">Graphic Design</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</main>
