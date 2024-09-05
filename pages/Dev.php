e9cfe4db39059b1cb58d4e3ca4243ef3d75749f6
KurtSchwimmbacher <231002@virtualwindow.co.za>
2024-09-05 16:08:38 +0200

    add title to dev page

--- pages/Dev.php
+++ pages/Dev.php
@@ -6,11 +6,23 @@
 <!-- include header -->
 <?php include_once '../includes/header.php'?>
 
+<!-- link css -->
+<link href="../css/Dev.css" rel="stylesheet">
+
 <main class="main-content">
     <div class="container">
         <div class="row">
             <div class="col-12 title-col mt-5">
                 <h1>Development Projects</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 mt-3">
+                <a class="github-link" href="https://github.com/KurtSchwimmbacher">
+                    <h2>My Github</h2>
+                </a>
+
+
             </div>
         </div>
 
